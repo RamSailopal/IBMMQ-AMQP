f47f01e3fa9da2b68bd65822e09c3c2f727b9811
RamSailopal <raman.sailopal@bytron.aero>
2023-11-14 11:06:27 +0000

    Topic needs to be specified

--- mount/send.sh
+++ mount/send.sh
@@ -5,4 +5,4 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /home/ram/node_modules/mqlight/samples
-node send.js -s amqp://app:test@localhost:5672
+node send.js -t Test -s amqp://app:test@localhost:5672
